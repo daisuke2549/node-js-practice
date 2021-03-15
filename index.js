7a2cc54c413d4d7eee7ab5c00e4ad96315e69292
daisuke sasaki <gerund25495641@gmail.com>
2021-03-15 22:05:51 +0900

    second commit

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ console.log("hello world");
 
 // npm = node package manager プロジェクトで利用するソフトウェアを管理する仕組み
 //同期処理、非同期処理
+//node js = サーバーサイドアプリケーション、クライアントサイドの開発もすることができる。
+//本だと情報の陳腐化が激しい。
 
 
 console.log("1000+200");
---
+++ js/main.js
@@ -0,0 +1,13 @@
+for ( let step = 0; step < 5; step++){
+    console.log("walk from west to east");
+}
+
+
+let i = 0;
+do{
+    i += 1;
+    console.log(i);
+}while(i <5);
+
+
+
